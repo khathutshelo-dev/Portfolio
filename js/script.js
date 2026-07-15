0c0a45aafc1bf2b1d6d966ee3e5aa7f4e039c489
Khathutshelo-dev <Khathuflashie@gmail.com>
2026-07-15 21:18:52 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -222,6 +222,6 @@ if (heroImg) {
 const menuToggle = document.querySelector(".menu-toggle");
 const nav = document.querySelector("nav");
 
-menuToggle.addEventListener("click", () => {
+menuToggle.addEventListener("click", function () {
     nav.classList.toggle("active");
 });
